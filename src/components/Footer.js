7972e5d8e94a3180ad6ce9017a30aced16fd1e2e
Vinitha Gowtheepan <sreevinithaa@gmail.com>
2022-07-05 13:34:22 +0800

    Update page layout

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// Here we import the Header.css file to grant access to some additional classNames
+import '../styles/Footer.css';
+
+function Footer() {
+    // TODO: Add a style attribute to header and the h1 element
+  
+    return (
+        <div className="footer">
+         <h4>© 2022 Vinitha Gowtheepan</h4>
+        
+        </div>
+    );
+  }
+  
+  export default Footer;
