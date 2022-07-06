1b59042cc849780e502d868212cbb7ba3c187aea
Vinitha Gowtheepan <sreevinithaa@gmail.com>
2022-07-06 15:54:15 +0800

    Update project page

---
+++ src/components/Project.js
@@ -0,0 +1,37 @@
+import React from "react";
+// Here we import the Navbar.css file to grant access to some additional classNames
+import "../styles/Project.css";
+
+function Project({
+  Url,
+  Repository,
+  imgUrl,
+  ProjectName,
+  ProjectDescription,
+  Technology,
+}) {
+  return (
+    <div class="project_item">
+      <a target="_blank" href={Url}>
+        <img src={imgUrl} alt={ProjectName} />{" "}
+      </a>
+      <h4>{ProjectName}</h4>
+      <h5>
+        <strong>Description :</strong>
+        {ProjectDescription}
+      </h5>
+      <h5>
+        <strong>Technology :</strong>
+        {Technology}
+      </h5>
+      <h5>
+        <strong>Repository :</strong>
+        <a target="_blank" href={Repository}>
+          {ProjectName}
+        </a>
+      </h5>
+    </div>
+  );
+}
+
+export default Project;
--- src/components/pages/PortFolio.js
+++ src/components/pages/PortFolio.js
@@ -1,13 +1,8 @@
 import React from 'react';
-
+import ProjectData from '../Data/Project.json';
+import Project from '../Project';
 export default function PortFolio() {
-  return (
-    <div>
-      <h1>PortFolio</h1>
-      <p>
-       I am Vinitha Gowtheepan.I am passionated and hardworking coder.I have backend experience back in home(Sri Lanka).Due to my family circumstance took a break from IT and moved to Australia.In the middle i missed my career and did one freelance project for one charity organisation.Then i realise i cant stay away from coding.Coding makes me happy.Then deside to develop my skills which makes me join FullStackDeveloper course in Boot Camp.
-       Now i am all ready to start my career again with added frontEnd development skills which makes me as fullstack developer.I am so exited to re-start my career in Australia.
-      </p>
-    </div>
-  );
+  return ProjectData.map((item, i) => (
+   <Project Url={item.Url} Repository={item.Repository} imgUrl={item.imgUrl} ProjectName={item.ProjectName} ProjectDescription={item.ProjectDescription} Technology={item.Technology}></Project>
+  ));
 }
