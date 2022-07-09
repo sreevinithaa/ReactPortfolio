19881fea80cfa3d38f206355b4205a136a3cb9ff
Vinitha Gowtheepan <sreevinithaa@gmail.com>
2022-07-09 12:18:19 +0800

    Update icon style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,12 +2,12 @@ import React from 'react';
 // Here we import the Header.css file to grant access to some additional classNames
 import '../styles/Footer.css';
 import myResume from "../components/Doc/CV_Vinitha_Gowtheepan.pdf";
-import { FaLinkedinIn,FaGithub,FaCloudDownloadAlt } from "react-icons/fa";
+import { FaLinkedinIn,FaGithub,FaStackOverflow } from "react-icons/fa";
 function Footer() {
    
     return (
         <div className="footer">
-         <a href='https://www.linkedin.com/in/vinitha-gowtheepan-a0a70926/' target="_blank"><FaLinkedinIn /></a><a href='https://github.com/sreevinithaa' target="_blank"><FaGithub /></a><a href={myResume} download="VinithaGowtheepan.pdf"><FaCloudDownloadAlt /></a>
+         <a href='https://www.linkedin.com/in/vinitha-gowtheepan-a0a70926/' target="_blank"><FaLinkedinIn size={30} /></a><a href='https://github.com/sreevinithaa' target="_blank"><FaGithub size={30}/></a><a href={myResume} download="VinithaGowtheepan.pdf"><FaStackOverflow size={30} /></a>
         
         </div>
     );
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,7 +1,7 @@
 import React from "react";
 // Here we import the Navbar.css file to grant access to some additional classNames
 import "../styles/Project.css";
-
+import { FaGitSquare,FaStaylinked } from "react-icons/fa";
 function Project({
   Url,
   Repository,
@@ -15,9 +15,15 @@ function Project({
     
 
     <div className="project_item">
-      <a target="_blank" href={Url}>
-        <img src={require(`${imgUrl}`)} alt={ProjectName} />{" "}
-      </a>
+
+<div class="imgcontainer">
+<img src={require(`${imgUrl}`)} class="image"  alt={ProjectName} />{" "}
+  <div class="overlay">
+    <div class="text"><a target="_blank" href={Repository}><FaGitSquare size={30} /></a><a target="_blank" href={Url}><FaStaylinked size={30}> </FaStaylinked>  </a></div>
+  </div>
+</div>
+      
+ 
       <h4>{ProjectName}</h4>
       <h5>
         <strong>Description :</strong>
@@ -27,12 +33,7 @@ function Project({
         <strong>Technology :</strong>
         {Technology}
       </h5>
-      <h5>
-        <strong>Repository :</strong>
-        <a target="_blank" href={Repository}>
-          {ProjectName}
-        </a>
-      </h5>
+      
     </div>
   );
 }
